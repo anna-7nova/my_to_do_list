0e79c3185d9f863b54bc2c890c188d01b6b0e22a
anna-7nova <anna.7nova@yandex.by>
2024-12-03 23:57:56 +0300

    add small part of  styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,21 @@
 import React from 'react';
 import './App.css';
+import { TodoList } from './components/TodoList';
+
+//CRUD
+// - repeat, duplicate
 
 function App() {
+    // business logic
+
+    const todolistTitle_1 = "Why to learn";
+
+
+    //UI
     return (
         <div className="App">
-            <div>
-                <h3>What to learn</h3>
-                <div>
-                    <input/>
-                    <button>+</button>
-                </div>
-                <ul>
-                    <li><input type="checkbox" checked={true}/> <span>HTML&CSS</span></li>
-                    <li><input type="checkbox" checked={true}/> <span>JS</span></li>
-                    <li><input type="checkbox" checked={false}/> <span>React</span></li>
-                </ul>
-                <div>
-                    <button>All</button>
-                    <button>Active</button>
-                    <button>Completed</button>
-                </div>
-            </div>
+            <TodoList />
+            <TodoList />
         </div>
     );
 }
