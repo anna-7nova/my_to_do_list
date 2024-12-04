06d6be7b6f227a1132cb20171642ea208f633d01
anna-7nova <anna.7nova@yandex.by>
2024-12-04 23:57:13 +0300

    create the structure of main component

--- src/App.tsx
+++ src/App.tsx
@@ -4,18 +4,60 @@ import { TodoList } from './components/TodoList';
 
 //CRUD
 // - repeat, duplicate
+// - create clear structure
+export type TaskType = {
+    id: number
+    title: string
+    isDone: boolean
+}
 
 function App() {
     // business logic
 
-    const todolistTitle_1 = "Why to learn";
+    const todolistTitle_1 = "What to learn"
+    const todolistTitle_2 = 'What to watch'
+
+    const tasks_1: Array<TaskType> = [
+        {
+            id: 1,
+            title: "HTML&CSS",
+            isDone: true
+        },
+        {
+            id: 2,
+            title: "JS",
+            isDone: true
+        },
+        {
+            id: 3,
+            title: "React",
+            isDone: false
+        },
+    ]
+    const tasks_2: TaskType[] = [
+        {
+            id: 4,
+            title: "One alone",
+            isDone: true
+        },
+        {
+            id: 5,
+            title: "Grinch",
+            isDone: false
+        },
+        {
+            id: 6,
+            title: "The sole",
+            isDone: false
+        },
+    ]
 
 
     //UI
     return (
         <div className="App">
-            <TodoList />
-            <TodoList />
+            <TodoList title={todolistTitle_1} tasks={tasks_1}/>
+            <TodoList title={todolistTitle_2} tasks={tasks_2}/>
         </div>
     );
 }
---
+++ src/components/FiltersButtons.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Button } from './Button';
+
+export const FiltersButtons = () => {
+    return (
+        <div>
+                <Button title='All'/>
+                <Button title='Active'/>
+                <Button title='Completed'/>
+            </div>
+    );
+}
+
+
